76b1b652a040d7dca432b445cd06e4d190f8bced
Leanne Northrop <leanne.northrop@gmail.com>
2016-01-11 17:29:34 +0000

    Implemented Till lookup price. Returns price in pence value for known product.

--- src/main/scala/org/northrop/leanne/shoppingcart/shop/Till.scala
+++ src/main/scala/org/northrop/leanne/shoppingcart/shop/Till.scala
@@ -4,4 +4,6 @@ import scala.collection.immutable._
 
 case class ProductPrice(val product : Product, val priceInPence : Int)
 
-case class Till(val prices : List[ProductPrice])
+case class Till(val prices : List[ProductPrice]) {
+  def lookupPrice(product : Product) : Option[Int] = prices.find(_.product.name == product.name).map(_.priceInPence)
+}
--- src/test/scala/org/northrop/leanne/shoppingcart/shop/TillSpec.scala
+++ src/test/scala/org/northrop/leanne/shoppingcart/shop/TillSpec.scala
@@ -6,6 +6,11 @@ import org.northrop.leanne.UnitSpec
     Behaviour tests for org.northrop.leanne.shoppingcart.shop.Till
 */
 class TillSpec extends UnitSpec {
+  trait TillObjects {
+    val prices = ProductPrice(Product("apple"), 33) :: List()
+    val till = Till(prices)
+  }
+
   "ProductPrice" should "initialise with Product and price in pence value" in {
       // setup
       val name = "apple"
@@ -16,11 +21,30 @@ class TillSpec extends UnitSpec {
       productPrice.priceInPence shouldBe 100
   }
 
-  "Till" should "initialise with List of ProductPrices" in {
+  "Till" should "initialise with List of ProductPrices" in new TillObjects {
+      // check
+      till.prices shouldBe prices
+  }
+
+  "Till lookupPrice" should "return Some price in pence value for a known Product" in new TillObjects {
       // setup
-      val prices = ProductPrice(Product("apple"), 33) :: ProductPrice(Product("orange"),44) :: List()
+      val product = Product("apple")
+
+      // do it
+      val priceOption = till.lookupPrice(product)
 
       // check
-      Till(prices).prices shouldBe prices
+      priceOption shouldBe Some(33)
+  }
+
+  "Till lookupPrice" should "return None for a unknown Product" in new TillObjects {
+      // setup
+      val product = Product("orange")
+
+      // do it
+      val priceOption = till.lookupPrice(product)
+
+      // check
+      priceOption shouldBe None
   }
 }
