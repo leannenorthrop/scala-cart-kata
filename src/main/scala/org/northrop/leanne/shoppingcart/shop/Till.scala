226fa094b8794864d35c62be03ff3c1160bf8b37
Leanne Northrop <leanne.northrop@gmail.com>
2016-01-11 17:37:49 +0000

    Implemented Till scan returning total price of given cart contents.

--- src/main/scala/org/northrop/leanne/shoppingcart/shop/Till.scala
+++ src/main/scala/org/northrop/leanne/shoppingcart/shop/Till.scala
@@ -7,3 +7,9 @@ case class ProductPrice(val product : Product, val priceInPence : Int)
 case class Till(val prices : List[ProductPrice]) {
   def lookupPrice(product : Product) : Option[Int] = prices.find(_.product.name == product.name).map(_.priceInPence)
 }
+
+object Till {
+  def scan(till:Till)(cart:Cart) : Int = {
+    cart.contents.map(product => product.flatMap(till.lookupPrice(_))).filter(_!=None).map(_.getOrElse(0)).foldLeft(0)(_ + _)
+  }
+}
--- src/test/scala/org/northrop/leanne/shoppingcart/shop/TillSpec.scala
+++ src/test/scala/org/northrop/leanne/shoppingcart/shop/TillSpec.scala
@@ -6,9 +6,16 @@ import org.northrop.leanne.UnitSpec
     Behaviour tests for org.northrop.leanne.shoppingcart.shop.Till
 */
 class TillSpec extends UnitSpec {
-  trait TillObjects {
+  trait TillWithoutOrangePriceObjects {
     val prices = ProductPrice(Product("apple"), 33) :: List()
     val till = Till(prices)
+    val scanner = Till.scan(till)_
+  }
+
+  trait TillObjects {
+    val prices = ProductPrice(Product("apple"), 33) :: ProductPrice(Product("orange"), 20) :: List()
+    val till = Till(prices)
+    val scanner = Till.scan(till)_
   }
 
   "ProductPrice" should "initialise with Product and price in pence value" in {
@@ -37,7 +44,7 @@ class TillSpec extends UnitSpec {
       priceOption shouldBe Some(33)
   }
 
-  "Till lookupPrice" should "return None for a unknown Product" in new TillObjects {
+  "Till lookupPrice" should "return None for a unknown Product" in new TillWithoutOrangePriceObjects {
       // setup
       val product = Product("orange")
 
@@ -46,5 +53,17 @@ class TillSpec extends UnitSpec {
 
       // check
       priceOption shouldBe None
+  }
+
+  "Till scan" should "return total value of known products" in new TillObjects {
+      // setup
+      val cartContents = "apple, abc, orange, , apple, apple"
+      val cart = Cart(cartContents)
+
+      // do it
+      val total = scanner(cart)
+
+      // check
+      total shouldBe 119
   }
 }
