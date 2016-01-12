7fe816e828c58e1d0aea110c8ecde7b4e26dfb39
Leanne Northrop <leanne.northrop@gmail.com>
2016-01-12 12:43:14 +0000

    Removing old base unit spec class.

--- src/test/scala/UnitSpec.scala
+++
@@ -1,6 +0,0 @@
-package org.northrop.leanne.shopping
-
-import org.scalatest._
-import org.scalamock.scalatest.MockFactory
-
-abstract class UnitSpec extends FlatSpec with Matchers with OptionValues with Inside with Inspectors with MockFactory
